8ab203c0cbc52cc162832eb660c974a97c5208bb
Manikanta Peddi <Manikantapeddi24@gmail.com>
2026-07-01 19:20:40 +0530

    Modified Employee Salary

--- src/main/java/com/example/employee_management_api/service/EmployeeService.java
+++ src/main/java/com/example/employee_management_api/service/EmployeeService.java
@@ -13,8 +13,8 @@ public class EmployeeService {
 
     // Temporary in-memory list to simulate a database table
     private final List<Employee> employeeList = new ArrayList<>(List.of(
-    new Employee(1, "Alice", "Engineering", 75000),
-    new Employee(2, "Bob", "HR", 55000)
+    new Employee(1, "Manikanta", "Engineering", 175000),
+    new Employee(2, "Jai Sri Ram", "HR", 155000)
 ));
 
 
